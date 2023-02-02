db33481753c7f189e033ddf1b840f05be0dca47f
Marina <marinalcaraujo@gmail.com>
2023-02-02 17:54:54 -0300

    atualizacao

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+let botaoEl = document.querySelectorAll('.botoes');
+let botaoFechar = document.querySelectorAll('#fechar');
 
+let secaoPorQueEl = document.querySelector('#secao-porque');
+let secaoSobreMimEl = document.querySelector('#secao-sobremim');
+let secaoExpectativasEl = document.querySelector('#secao-expectativas');
+
+let fundoEl = document.querySelector('main');
+
+let botoes = [
+    {
+        container: secaoPorQueEl,
+    },
+    {
+        container: secaoSobreMimEl,
+    },
+    {
+        container: secaoExpectativasEl,
+    },
+];
+
+for (let i = 0; i < botaoEl.length; i++) {
+    botaoEl[i].addEventListener('click', () => {
+      botoes[i].container.classList.remove('invisivel');
+      fundoEl.classList.add('invisivel');
+    });
+  
+    fechar[i].addEventListener('click', () => {
+      botoes[i].container.classList.add('invisivel');
+      fundoEl.classList.remove('invisivel');
+    });
+  }
